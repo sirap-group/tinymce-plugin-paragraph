665480cb7931acc779148e357cd840d5c4467bbb
Rémi Becheras <rbecheras@sirap.fr>
2015-11-26 12:07:04 +0100

    create plugin main file

---
+++ plugin.js
@@ -0,0 +1,15 @@
+/**
+ * plugin.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 2015 SIRAP SAS All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*global tinymce:true */
+
+tinymce.PluginManager.add('paragraph', function(editor) {
+
+});
