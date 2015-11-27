0e5bf616c0214f037cd731cf622b0572fd2f3980
Rémi Becheras <rbecheras@sirap.fr>
2015-11-27 13:38:25 +0100

    change DIV blocks in P on their content changes

--- plugin.js
+++ plugin.js
@@ -14,11 +14,9 @@ tinymce.PluginManager.add('paragraph', function(editor) {
 
 	function createColorPickAction() {
 		var colorPickerCallback = editor.settings.color_picker_callback;
-
 		if (colorPickerCallback) {
 			return function() {
 				var self = this;
-
 				colorPickerCallback.call(
 					editor,
 					function(value) {
@@ -27,6 +25,43 @@ tinymce.PluginManager.add('paragraph', function(editor) {
 					self.value()
 				);
 			};
+		}
+	}
+	function openMainWin(){
+		var paragraph = editor.dom.getParent(editor.selection.getStart(),'p');
+		// console.log('paragraph',paragraph);
+
+		editor.windowManager.open({
+			title: "Paragraph properties",
+			// bodyType: 'tabpanel',
+			data: {
+				indent: editor.dom.getStyle(paragraph,'text-indent'),
+				linespacing: editor.dom.getStyle(paragraph,'line-height'),
+				padding: editor.dom.getStyle(paragraph,'padding'),
+				margin: editor.dom.getStyle(paragraph,'margin'),
+				borderwidth: editor.dom.getStyle(paragraph,'border-width'),
+				bordercolor: editor.dom.getStyle(paragraph,'border-color')
+			},
+			body: generalParagraphForm,
+			onsubmit: function(evt){
+				editor.dom.setStyle(paragraph,'text-indent',evt.data.indent);
+				editor.dom.setStyle(paragraph,'line-height',evt.data.linespacing);
+				editor.dom.setStyle(paragraph,'padding',evt.data.padding);
+				editor.dom.setStyle(paragraph,'margin',evt.data.margin);
+				editor.dom.setStyle(paragraph,'border-width',evt.data.borderwidth);
+				if (evt.data.borderwidth) {
+					editor.dom.setStyle(paragraph,'border-style','solid');
+				}
+				editor.dom.setStyle(paragraph,'border-color',evt.data.bordercolor);
+			}
+		});
+	}
+	function div2p(changeEvt){
+		if (changeEvt.lastLevel) {
+			var selectedParagraph = editor.dom.getParent(editor.selection.getStart(),'p,div');
+			if (selectedParagraph.tagName === 'DIV'){
+				editor.dom.rename(selectedParagraph,'p');
+			}
 		}
 	}
 
@@ -59,36 +94,9 @@ tinymce.PluginManager.add('paragraph', function(editor) {
 		]
 	};
 
-
-	function openMainWin(){
-		var paragraph = editor.dom.getParent(editor.selection.getStart(),'p');
-		// console.log('paragraph',paragraph);
-
-		editor.windowManager.open({
-			title: "Paragraph properties",
-			// bodyType: 'tabpanel',
-			data: {
-				indent: editor.dom.getStyle(paragraph,'text-indent'),
-				linespacing: editor.dom.getStyle(paragraph,'line-height'),
-				padding: editor.dom.getStyle(paragraph,'padding'),
-				margin: editor.dom.getStyle(paragraph,'margin'),
-				borderwidth: editor.dom.getStyle(paragraph,'border-width'),
-				bordercolor: editor.dom.getStyle(paragraph,'border-color')
-			},
-			body: generalParagraphForm,
-			onsubmit: function(evt){
-				editor.dom.setStyle(paragraph,'text-indent',evt.data.indent);
-				editor.dom.setStyle(paragraph,'line-height',evt.data.linespacing);
-				editor.dom.setStyle(paragraph,'padding',evt.data.padding);
-				editor.dom.setStyle(paragraph,'margin',evt.data.margin);
-				editor.dom.setStyle(paragraph,'border-width',evt.data.borderwidth);
-				if (evt.data.borderwidth) {
-					editor.dom.setStyle(paragraph,'border-style','solid');
-				}
-				editor.dom.setStyle(paragraph,'border-color',evt.data.bordercolor);
-			}
-		});
-	}
+	// change DIV blocks in P on their content changes
+	// don't transform outer div elements that can contains tables and paragraphs
+	editor.on('change',div2p);
 
 	editor.addMenuItem('paragraph', {
 		separator: 'before',
@@ -96,6 +104,5 @@ tinymce.PluginManager.add('paragraph', function(editor) {
 		context: 'format',
 		onclick: openMainWin
 	});
-
 
 });
