156ef0969faeec2cd45e101ad59e0aa96c77abfc
Rémi Becheras <rbecheras@sirap.fr>
2016-09-15 12:24:47 +0200

    implement units.setFormValueWithUnit() and units.setFormValueWithoutUnit()

--- src/lib/units.js
+++ src/lib/units.js
@@ -1,7 +1,9 @@
 'use strict'
 
 module.exports = {
-  getUnitValues: getUnitValues
+  getUnitValues: getUnitValues,
+  setFormValueWithUnit: setFormValueWithUnit,
+  setFormValueWithoutUnit: setFormValueWithoutUnit
 }
 
 function getUnitValues () {
@@ -10,4 +12,30 @@ function getUnitValues () {
     {text: 'cm', value: 'cm'},
     {text: 'mm', value: 'mm'}
   ]
+}
+
+function setFormValueWithUnit (dom, paragraph, formData, cssPropertyName, propertyName, defaultValue) {
+  if (defaultValue === undefined) {
+    defaultValue = '0'
+  }
+  var rawCssValue = dom.getStyle(paragraph, cssPropertyName)
+  if (rawCssValue !== '') {
+    var unitPropertyName = propertyName + 'Unit'
+    formData[propertyName] = rawCssValue.slice(0, rawCssValue.length - 2)
+    formData[unitPropertyName] = rawCssValue.slice(rawCssValue.length - 2, rawCssValue.length)
+  } else {
+    formData[propertyName] = defaultValue
+  }
+}
+
+function setFormValueWithoutUnit (dom, paragraph, formData, cssPropertyName, propertyName, defaultValue) {
+  if (defaultValue === undefined) {
+    defaultValue = ''
+  }
+  var rawCssValue = dom.getStyle(paragraph, cssPropertyName)
+  if (rawCssValue !== '') {
+    formData[propertyName] = rawCssValue
+  } else {
+    formData[propertyName] = defaultValue
+  }
 }
